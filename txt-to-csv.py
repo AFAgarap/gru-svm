f094f0b6a9f9b5a98bb4d9be45f5c88bbe1c8d6e
AFAgarap <abienfred.agarap@yahoo.com>
2017-06-09 18:57:20 +0800

    Final program for converting TXT to CSV, with comment documentation

--- txt-to-csv.py
+++ txt-to-csv.py
@@ -3,29 +3,39 @@ import os
 from os import walk
 
 path = '/home/darth/Documents/Adamson University/CS Research Project 1/Kyoto 2013 Dataset/Kyoto2016/2013/'
+csv_path = '/home/darth/Documents/Adamson University/CS Research Project 1/Kyoto 2013 Dataset/CSV'
 
-months = []	# list to store the subdirectories (months 01-12) under the <path>
-data = []	# list to store the days under the subdirectories
+data = []	# list to store the filenames under the subdirectories of the <path>
+csv_data = []	# list to store the converted CSV files
 
 for (dirpath, dirnames, filenames) in walk(path):
-	''' append to <months> list the subdirectories under <path> '''
-	months.extend(os.path.join(dirpath, dirname) for dirname in dirnames)
-	break
+	''' Append to the list the filenames under the subdirectories of the <path> '''
+	data.extend(os.path.join(dirpath, filename) for filename in filenames)
 
-for index in range(len(months)):
-	''' loops through the <months> list from 01-12 '''
-	for (dirpath, dirnames, filenames) in walk(months[index]):
-		''' append to <data> list the filenames under each subdirectories of the <path> '''
-		data.extend(os.path.join(dirpath, filename) for filename in filenames)
+# Create the <csv_path> if it does not exist
+os.makedirs(csv_path) if not os.path.exists(csv_path) else print('CSV folder exists')
+
+for month in range(12):
+	''' Create the subdirectories under the <csv_path> if it does not exist '''
+	if next(walk(csv_path))[1].__len__() == 12:
+		print('Folders exist')
 		break
+	print('Creating subdirectories.')
+	# get the dirpath from the generator object <walk> (index 0)
+	# then joins the dirpath with the month number
+	os.makedirs(os.path.join(next(walk(csv_path))[0], '0' + str(month + 1) if month < 9 else str(month + 1)))
+	
 
+for index in range(len(data)):
+	''' Store the processed CSV filename to <csv_data> list '''
+	csv_data.append(os.path.join(csv_path, data[index].split(path)[1].replace('txt', 'csv')))
 
 for index in range(len(data)):
 	''' Reading the text files delimited with tab, and converts it to CSV '''
 	try:
 		print('Processing: {}'.format(data[index]))
 		in_csv = csv.reader(open(data[index], 'r'), delimiter='\t')
-		out_csv = csv.writer(open(csv_data[index], 'w'))
+		out_csv = csv.writer(open(csv_data[index], 'x'))
 		out_csv.writerows(in_csv)
 	except:
 		print('File not found: {}'.format(data[index]))
