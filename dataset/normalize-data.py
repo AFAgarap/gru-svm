f3974f71e665e2b1ee953b820067a05994f637dc
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-11 19:56:12 +0800

    Wrote other methods to be used for normalizing data: scale_boolean_values(), scale_label(), convert_time_to_integer()

--- dataset/normalize-data.py
+++ dataset/normalize-data.py
@@ -23,5 +23,25 @@ def linear_scale(min, max, x):
 		scaled_value = -1
 	return scaled_value
 
+def scale_boolean_values(value):
+	'''Scales the supposed boolean values in the dataset'''
+	if (value != 0):
+		return 1
+	elif (value == 0):
+		return 0
+
+def scale_label(label):
+	'''Scales the labels [-2 -1 1] -> [0, 1]'''
+	if (label == -2 or label == -1):
+		return 1	# -2 and -1 represents an attack
+	elif (label == 1):
+		return 0	# 1 represents normal session
+
+def convert_time_to_integer(time):
+	'''Converts time to integer value, preparation for scaling'''
+	h, m, s = map(int, time.split(':'))
+	parsed_time = h + (m * (1 / 60)) + (s * (1 / 3600))
+	return parsed_time
+
 if __name__ == '__main__':
 	main()
