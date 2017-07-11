22ada60825abdacafa7ef87a3d89a3fa08423e9c
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-11 19:07:56 +0800

    Removed the + 0.0, since it's negligible

--- dataset/normalize-data.py
+++ dataset/normalize-data.py
@@ -18,7 +18,7 @@ def list_files(path):
 def linear_scale(min, max, x):
 	'''Scales integer values [min, max] -> [0.0, 1.0]'''
 	if (x >= min and x <= max):
-		scaled_value = (((1.0 - 0.0) * (x - min)) / (max - min)) + 0.0
+		scaled_value = (((1.0 - 0.0) * (x - min)) / (max - min))
 	else:
 		scaled_value = -1
 	return scaled_value
