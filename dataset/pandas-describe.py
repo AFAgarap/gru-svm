4beb106a95434464813c5508107333a435205e48
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-14 18:10:03 +0800

    Added standardization of  for test

--- dataset/pandas-describe.py
+++ dataset/pandas-describe.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from os import walk
+from sklearn import preprocessing
 
 PATH = '/home/darth/Desktop/pandas'
 
@@ -16,7 +17,24 @@ def main():
 	for file in files:
 		data = data.append(pd.read_csv(filepath_or_buffer=file, header=None, engine='python'))
 	
-	print(data.describe())
+	data[22] = data[22].apply(lambda time: int(time.split(':')[0]) +
+		(int(time.split(':')[1]) * (1 / 60)) +
+		(int(time.split(':')[2]) * (1 / 3600)))
+
+	std_scale = preprocessing.StandardScaler().fit(data[0])
+	data_std = std_scale.transform(data[0])
+
+	print('Mean after standardization: \ndata[0]={}'.format(data_std.mean()))
+	print('Std dev after standardization: \ndata[0]={}'.format(data_std.std()))
+
+	print(data_std)
+	# print(data.describe())
+
+def standardize(x):
+	mean = sum(x) / len(x)
+	std_dev = (1 / len(x) * sum([ (x_i - mean) ** 2 for x_i in x] )) ** 0.5
+	z_score = [ (x_i - mean) / std_dev for x_i in x ]
+	return z_score
 
 def list_files(path):
 	file_list = []
