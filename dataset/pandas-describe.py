d3c5841a9eda198bd5592d38873fd8a1f7e7e0a6
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-17 21:01:11 +0800

    Standardized continuous and quasi-continuous, and Indexed the categorical data

--- dataset/pandas-describe.py
+++ dataset/pandas-describe.py
@@ -19,6 +19,9 @@ cols_to_std = ['duration', 'src_bytes', 'dest_bytes', 'count',
 				'dst_host_srv_serror_rate', 'src_port_num',
 				'dst_port_num', 'start_time']
 
+cols_to_index = ['ashula_detection', 'dst_ip_add', 'flag', 'ids_detection', 'label',
+					'malware_detection', 'protocol', 'service', 'src_ip_add']
+
 def main():
 	# get all the CSV files in the PATH dir
 	files = list_files(path=PATH)
@@ -27,22 +30,17 @@ def main():
 	df = pd.DataFrame(dtype=str)
 
 	# append the dfs from each file to the data df
-	# for file in files:
-	for index in range(0, 1):
-		df = df.append(pd.read_csv(filepath_or_buffer=files[index], names=col_names, engine='python'))
+	for file in files:
+		df = df.append(pd.read_csv(filepath_or_buffer=file, names=col_names, engine='python'))
 	
 	df['start_time'] = df['start_time'].apply(lambda time: int(time.split(':')[0]) +
-		(int(time.split(':')[1]) * (1 / 60)) +
-		(int(time.split(':')[2]) * (1 / 3600)))
+											(int(time.split(':')[1]) * (1 / 60)) +
+											(int(time.split(':')[2]) * (1 / 3600)))
 
+	df[cols_to_index] = df[cols_to_index].apply(preprocessing.LabelEncoder().fit_transform)
 	df[cols_to_std] = preprocessing.StandardScaler().fit_transform(df[cols_to_std])
 	
 	print(df)
-
-	# Printing of mean and std dev
-	# for index in range(len(col_names)):
-	# 	print('Mean after standardization: \tdata[{}]={}'.format(col_names[index], df[[col_names[index]]].mean()))
-	# 	print('Std dev after standardization: \tdata[{}]={}'.format(col_names[index], df[[col_names[index]]].std()))
 
 def list_files(path):
 	file_list = []
