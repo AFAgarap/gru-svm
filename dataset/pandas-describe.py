ded339ae24e8e319313a1c94aa4fc862731722a5
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-17 13:28:10 +0800

    Module to standardize continuous and quasi-continuous data

--- dataset/pandas-describe.py
+++ dataset/pandas-describe.py
@@ -6,35 +6,43 @@ from sklearn import preprocessing
 
 PATH = '/home/darth/Desktop/pandas'
 
+col_names = ['duration', 'service', 'src_bytes', 'dest_bytes', 'count', 'same_srv_rate',
+			'serror_rate', 'srv_serror_rate', 'dst_host_count', 'dst_host_srv_count',
+			'dst_host_same_src_port_rate', 'dst_host_serror_rate', 'dst_host_srv_serror_rate',
+			'flag', 'ids_detection', 'malware_detection', 'ashula_detection', 'label', 'src_ip_add',
+			'src_port_num', 'dst_ip_add', 'dst_port_num', 'start_time', 'protocol']
+
+cols_to_std = ['duration', 'src_bytes', 'dest_bytes', 'count',
+				'same_srv_rate', 'serror_rate', 'srv_serror_rate',
+				'dst_host_count', 'dst_host_srv_count',
+				'dst_host_same_src_port_rate', 'dst_host_serror_rate',
+				'dst_host_srv_serror_rate', 'src_port_num',
+				'dst_port_num', 'start_time']
+
 def main():
 	# get all the CSV files in the PATH dir
 	files = list_files(path=PATH)
 
 	# create empty df, where dfs shall be appended
-	data = pd.DataFrame(dtype=str)
+	df = pd.DataFrame(dtype=str)
 
 	# append the dfs from each file to the data df
-	for file in files:
-		data = data.append(pd.read_csv(filepath_or_buffer=file, header=None, engine='python'))
+	# for file in files:
+	for index in range(0, 1):
+		df = df.append(pd.read_csv(filepath_or_buffer=files[index], names=col_names, engine='python'))
 	
-	data[22] = data[22].apply(lambda time: int(time.split(':')[0]) +
+	df['start_time'] = df['start_time'].apply(lambda time: int(time.split(':')[0]) +
 		(int(time.split(':')[1]) * (1 / 60)) +
 		(int(time.split(':')[2]) * (1 / 3600)))
 
-	std_scale = preprocessing.StandardScaler().fit(data[0])
-	data_std = std_scale.transform(data[0])
+	df[cols_to_std] = preprocessing.StandardScaler().fit_transform(df[cols_to_std])
+	
+	print(df)
 
-	print('Mean after standardization: \ndata[0]={}'.format(data_std.mean()))
-	print('Std dev after standardization: \ndata[0]={}'.format(data_std.std()))
-
-	print(data_std)
-	# print(data.describe())
-
-def standardize(x):
-	mean = sum(x) / len(x)
-	std_dev = (1 / len(x) * sum([ (x_i - mean) ** 2 for x_i in x] )) ** 0.5
-	z_score = [ (x_i - mean) / std_dev for x_i in x ]
-	return z_score
+	# Printing of mean and std dev
+	# for index in range(len(col_names)):
+	# 	print('Mean after standardization: \tdata[{}]={}'.format(col_names[index], df[[col_names[index]]].mean()))
+	# 	print('Std dev after standardization: \tdata[{}]={}'.format(col_names[index], df[[col_names[index]]].std()))
 
 def list_files(path):
 	file_list = []
