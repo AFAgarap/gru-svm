d4b74808dd2771455b40a39e7c63c4d572709208
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-26 20:47:49 +0800

    Added comment documentation, and checking if there are any anomaly in the data, i.e. misclassified if (1) attack or (2) normal

--- dataset/check_standardized_data.py
+++ dataset/check_standardized_data.py
@@ -1,17 +1,25 @@
 import os
+import pandas as pd
 import tensorflow as tf
+import standardize_data
 
-PATH = '/home/darth/Desktop/preprocessed/train/3'
+# the path of the dataset to check
+PATH = '/home/darth/GitHub Projects/gru_svm/dataset/train/5/normal'
 
+# list to contain the filenames in PATH
 files = []
 
+# append the files under PATH to the list files[]
 for dirpath, dirnames, filenames in os.walk(PATH):
 	files.extend(os.path.join(dirpath, filename) for filename in filenames)
 
+# TF queue to contain the list of files
 filename_queue = tf.train.string_input_producer(files)
 
+# TF reader
 reader = tf.TextLineReader()
 
+# default values, in case of empty columns
 record_defaults = [[1.0] for x in range(24)]
 
 key, value = reader.read(filename_queue)
@@ -33,3 +41,26 @@ with tf.Session() as sess:
 
 	coord.request_stop()
 	coord.join(threads)
+
+print('----')
+
+# dataframe to contain the desired dataset
+df = pd.DataFrame()
+
+# dataframe to contain the wrong dataset
+# this will only be used for checking
+# the result must be __len__() == 0
+df_n = pd.DataFrame()
+
+for file in files:
+	df = df.append(pd.read_csv(filepath_or_buffer=file, names=standardize_data.col_names, engine='python'))
+
+print('Done appending...')
+
+# check if there are any label
+# that does not belong to the df
+df_n = df[df['label'] == 1]
+
+# if there are no outcast data
+# the splitting of dataset was successful
+print('Success' if df_n.__len__() == 0 else 'An unexpected error occurred.')
