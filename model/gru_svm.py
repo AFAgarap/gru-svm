1ac0aa3cb7e6435066953f22c49a06342b84170b
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-02 16:31:21 +0800

    Loss is now decreasing, but accuracy is stuck at 50%

--- model/gru_svm.py
+++ model/gru_svm.py
@@ -1,3 +1,6 @@
+# algorithms to check co-linearity between variables
+# identify which variables can be removed from features
+
 import argparse
 import data
 import numpy as np
@@ -11,9 +14,9 @@ NLAYERS = 3
 SVMC = 1
 learning_rate = 0.01
 
-CKPT_PATH = 'ckpt/gru_svm'
+CKPT_PATH = 'ckpt/gru_svm/'
 MODEL_NAME = 'gru_svm'
-TRAIN_PATH = '/home/darth/GitHub Projects/gru_svm/dataset/train/6'
+TRAIN_PATH = '/home/darth/GitHub Projects/gru_svm/dataset/train'
 TEST_PATH = '/home/darth/GitHub Projects/gru_svm/dataset/test'
 
 SESSION_CONFIG = tf.ConfigProto(
@@ -48,9 +51,16 @@ def main():
 	bias = tf.Variable(tf.constant(0.1, shape=[2]), name='bias')
 	logits = tf.matmul(last, weight) + bias
 
+	# prediction = tf.nn.softmax(logits)
+	# loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_input)
+	# train_step = tf.train.AdagradOptimizer(0.1).minimize(loss)
 	regularization_loss = 0.5 * tf.reduce_sum(tf.square(weight))
 	hinge_loss = tf.reduce_sum(tf.maximum(tf.zeros([BATCH_SIZE, 1]), 1 - y * logits))
 	loss = regularization_loss + SVMC * hinge_loss
+
+	predicted_class = tf.sign(logits)
+	correct_prediction = tf.equal(y, predicted_class)
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 	train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
@@ -69,8 +79,8 @@ def main():
 		threads = tf.train.start_queue_runners(coord=coord)
 
 		try:
-			for index in range(10):
-				# for j in range(1000):
+
+			for index in range(100):
 				example_batch, label_batch, key_batch = sess.run([examples, labels, keys])
 				_, train_loss_ = sess.run([train_step, loss],
 					feed_dict = { x : example_batch[..., np.newaxis],
@@ -89,5 +99,8 @@ def main():
 		coord.join(threads)
 		saver = tf.train.Saver()
 		saver.save(sess, CKPT_PATH + MODEL_NAME, global_step=index)
+
+		print('Accuracy : {}'.format(sess.run(accuracy,
+			feed_dict={ x : example_batch[..., np.newaxis], y_input : label_batch, Hin : np.zeros([BATCH_SIZE, CELLSIZE * NLAYERS])})))
 
 main()
