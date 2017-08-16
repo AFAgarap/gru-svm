054db273970bbd8f421f1009dcac0426bd8911e1
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-16 18:24:18 +0800

    Saves the dataframe to CSV to chunks of files, removed columns dst_ip_add and src_ip_add

--- dataset/bin_data.py
+++ dataset/bin_data.py
@@ -15,12 +15,18 @@
 
 """Bins continuous data into 10 evenly-spaced intervals"""
 import numpy as np
+import os
 import pandas as pd
 import standardize_data as sd
 import tensorflow as tf
 
+NUM_CHUNKS = 10
 PATH = '/home/darth/GitHub Projects/gru_svm/backup/5'
+WRITE_PATH = '/home/darth/GitHub Projects/gru_svm/dataset/train/foo'
 column_names = sd.col_names
+columns_to_save = list(column_names)
+columns_to_save.remove('dst_ip_add')
+columns_to_save.remove('src_ip_add')
 cols_to_std = sd.cols_to_std
 cols_to_std.append('service')
 cols_to_std.append('flag')
@@ -37,3 +43,7 @@ df = df.drop(labels=['dst_ip_add', 'src_ip_add'], axis=1)
 for index in range(len(cols_to_std)):
     bins = np.linspace(df[cols_to_std[index]].min(), df[cols_to_std[index]].max(), 10)
     df[cols_to_std[index]] = np.digitize(df[cols_to_std[index]], bins, right=True)
+
+for id, df_i in enumerate(np.array_split(df, NUM_CHUNKS)):
+	df_i.to_csv(path_or_buf=os.path.join(WRITE_PATH, '{id}.csv'.format(id=id)), columns=columns_to_save, header=None, index=False)
+	print('Saving CSV file : {path}'.format(path=os.path.join(WRITE_PATH, '{id}'.format(id=id))))
