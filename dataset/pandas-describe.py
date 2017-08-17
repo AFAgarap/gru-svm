2e0406c7a1dadaab7bcab8279ff474e8e6465f62
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-17 17:57:34 +0800

    Added Apache 2.0 License, added module description.

--- dataset/pandas-describe.py
+++ dataset/pandas-describe.py
@@ -1,3 +1,19 @@
+# Copyright 2017 Abien Fred Agarap. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Module for getting the dataset description based on sample data"""
 import os
 import pandas as pd
 import standardize_data
@@ -17,7 +33,7 @@ df = pd.DataFrame()
 # append the filenames to the list
 for (dirpath, dirnames, filenames) in os.walk(PATH):
 	files.extend(os.path.join(dirpath, filename) for filename in filenames)
-	print('Appending file : {}'.format(file))
+	
 
 # append the data from files to df
 for file in files:
