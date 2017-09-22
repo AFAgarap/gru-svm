d46489c03d0f12ef00aa2bd387dc65a3ab3feb21
AFAgarap <abienfred.agarap@yahoo.com>
2017-09-22 17:56:06 +0800

    Starting to rewrite the SVM functional implementation to OOP implementation

--- model/svm.py
+++ model/svm.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = 'Abien Fred Agarap'
 
 import argparse
@@ -39,16 +39,19 @@ SEQUENCE_LENGTH = 21
 SVM_C = 1
 
 
-def variable_summaries(var):
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
+class Svm:
+    
+    @staticmethod
+    def variable_summaries(var):
+        with tf.name_scope('summaries'):
+            mean = tf.reduce_mean(var)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)
 
 
 def train_model(train_data, test_data, checkpoint_path, log_path, model_name):
