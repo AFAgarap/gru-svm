24345428b0be2dc1be4e3bd824454f7a71ba56e0
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-09-25 20:54:58 +0800

    Now classifies the test data by batch, and saves the predicted and actual labels as result

--- model/svm_classifier.py
+++ model/svm_classifier.py
@@ -21,12 +21,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Abien Fred Agarap'
 
 import argparse
 from data import load_data
 import numpy as np
+import os
 import tensorflow as tf
 
 BATCH_SIZE = 256
@@ -34,10 +35,13 @@ CELL_SIZE = 256
 NUM_CLASSES = 2
 
 
-def predict(test_data, checkpoint_path, result_filename):
+def predict(test_data, checkpoint_path, result_path):
     """Classifies the data whether there is an attack or none"""
 
     test_features, test_labels = load_data(dataset=test_data)
+
+    test_size = test_features.shape[0]
+    print(test_size)
 
     # variables initializer
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
@@ -56,21 +60,23 @@ def predict(test_data, checkpoint_path, result_filename):
             print('Loaded model from {}'.format(tf.train.latest_checkpoint(checkpoint_path)))
 
         try:
-            for _ in range(1):
-                # todo Get test examples and test labels by batch
+            for step in range(test_size // BATCH_SIZE):
+                offset = (step * BATCH_SIZE) % test_size
+                test_example_batch = test_features[offset:(offset + BATCH_SIZE)]
+                test_label_batch = test_labels[offset:(offset+BATCH_SIZE)]
 
                 # dictionary for input values for the tensors
-                feed_dict = {'input/x_input:0': test_example_batch[:200]}
+                feed_dict = {'input/x_input:0': test_example_batch}
 
                 # get the tensor for classification
                 svm_tensor = sess.graph.get_tensor_by_name('accuracy/prediction:0')
                 predictions = sess.run(svm_tensor, feed_dict=feed_dict)
 
-                label_onehot = tf.one_hot(test_label_batch[:200], NUM_CLASSES, 1.0, -1.0)
+                label_onehot = tf.one_hot(test_label_batch, NUM_CLASSES, 1.0, -1.0)
                 y_onehot = sess.run(label_onehot)
 
                 # add key, value pair for labels
-                feed_dict['input/y_input:0'] = y_onehot
+                feed_dict['input/y_input:0'] = test_label_batch
 
                 # get the tensor for calculating the classification accuracy
                 accuracy_tensor = sess.graph.get_tensor_by_name('accuracy/accuracy/Mean:0')
@@ -85,22 +91,23 @@ def predict(test_data, checkpoint_path, result_filename):
                 print('Accuracy : {}'.format(accuracy))
 
                 # save the full array
-                np.savetxt(result_filename, X=prediction_and_actual, fmt='%.8f', delimiter=',', newline='\n')
+                np.savetxt(os.path.join(result_path, 'svm-{}-result.csv'.format(step)), X=prediction_and_actual,
+                           fmt='%.1f', delimiter=',', newline='\n')
         except tf.errors.OutOfRangeError:
             print('EOF')
         except KeyboardInterrupt:
-            print('KeyboardInterrupt')
+            print('KeyboardInterrupt at step {}'.format(step))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='SVM Classifier')
     group = parser.add_argument_group('Arguments')
     group.add_argument('-d', '--dataset', required=True, type=str,
-                       help='path of the dataset to be classified')
+                       help='the NumPy array dataset (*.npy) to be classified')
     group.add_argument('-m', '--model', required=True, type=str,
                        help='path of the trained model')
-    group.add_argument('-r', '--result', required=True, type=str,
-                       help='filename for the saved result')
+    group.add_argument('-r', '--result_path', required=True, type=str,
+                       help='path where to save the actual and predicted labels')
     arguments = parser.parse_args()
     return arguments
 
@@ -108,4 +115,4 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    predict(args.dataset, args.model, args.result)
+    predict(args.dataset, args.model, args.result_path)
