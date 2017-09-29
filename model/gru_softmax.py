f324f30e88533418a4f978e3c0bdd3ad7e0f3e60
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-09-29 17:26:52 +0800

    Changed the class file docstring

--- model/gru_softmax.py
+++ model/gru_softmax.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ==============================================================================
 
-"""Implementation of GRU+Softmax model for Intrusion Detection"""
+"""Implementation of GRU+Softmax model"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
