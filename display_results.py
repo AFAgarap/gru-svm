710421c8aca0942d20d3343e5711ea2cb48e0aa1
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-09-30 18:31:02 +0800

    tf.argmax() is needed to convert the one-hot encoded results, added a display of which CSV file has been appended to the dataframe

--- display_results.py
+++ display_results.py
@@ -24,11 +24,12 @@ from __future__ import print_function
 __version__ = '0.1'
 __author__ = 'Abien Fred Agarap'
 
+from dataset.normalize_data import list_files
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
-from dataset.normalize_data import list_files
+import tensorflow as tf
 
 TRAINING_RESULTS_PATH = '/home/darth/GitHub Projects/gru_svm/results/gru_svm/training'
 VALIDATION_RESULTS_PATH = '/home/darth/GitHub Projects/gru_svm/results/gru_svm/validation'
@@ -41,12 +42,17 @@ def view_results(path):
 
     for file in files:
         df = df.append(pd.read_csv(filepath_or_buffer=file, header=None))
-
+        print('appending {}'.format(file))
+        
     results = np.array(df)
 
     predictions = results[:, :2]
 
     actual = results[:, 2:]
+
+    with tf.Session() as sess:
+        predictions = sess.run(tf.argmax(predictions, 1))
+        actual = sess.run(tf.argmax(actual, 1))
 
     conf = confusion_matrix(y_true=actual, y_pred=predictions)
 
