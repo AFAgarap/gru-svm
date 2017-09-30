6fc23fb717df9fcacccf49fe2bc793eb53e900b9
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-09-30 11:58:08 +0800

    Wrote the method for writing the predicted and actual labels array to file, save_labels()

--- models/gru_softmax/gru_softmax.py
+++ models/gru_softmax/gru_softmax.py
@@ -235,3 +235,17 @@ class GruSoftmax:
             tf.summary.scalar('max', tf.reduce_max(var))
             tf.summary.scalar('min', tf.reduce_min(var))
             tf.summary.histogram('histogram', var)
+
+    @staticmethod
+    def save_labels(predictions, actual, result_path, step):
+        """Saves the actual and predicted labels to a CSV file"""
+
+        # concatenate the predicted labels and actual labels
+        prediction_and_actual = np.array([predictions, actual], dtype=np.int8)
+
+        # transpose the axes
+        prediction_and_actual = prediction_and_actual.T
+
+        # save every prediction_and_actual numpy array to a CSV file for analysis purposes
+        np.savetxt(os.path.join(result_path, 'gru_svm-{}-training.csv'.format(step)),
+                   X=prediction_and_actual, fmt='%.3f', delimiter=',', newline='\n')
