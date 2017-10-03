d784c1d2be44e4997a34011431307ef00ed39646
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-03 21:58:36 +0800

    Added a docstring to normalize_data()

--- dataset/normalize_data.py
+++ dataset/normalize_data.py
@@ -20,7 +20,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__ = 'Abien Fred Agarap'
 
 import argparse
@@ -32,14 +32,14 @@ from sklearn import preprocessing
 
 
 # column names of 24 features
-col_names = ['duration', 'service', 'src_bytes', 'dest_bytes', 'count', 'same_srv_rate',
+COLUMN_NAMES = ['duration', 'service', 'src_bytes', 'dest_bytes', 'count', 'same_srv_rate',
              'serror_rate', 'srv_serror_rate', 'dst_host_count', 'dst_host_srv_count',
              'dst_host_same_src_port_rate', 'dst_host_serror_rate', 'dst_host_srv_serror_rate',
              'flag', 'ids_detection', 'malware_detection', 'ashula_detection', 'label', 'src_ip_add',
              'src_port_num', 'dst_ip_add', 'dst_port_num', 'start_time', 'protocol']
 
 # column names of continuous and quasi-continuous features
-cols_to_std = ['duration', 'src_bytes', 'dest_bytes', 'count',
+COLUMN_TO_STANDARDIZE = ['duration', 'src_bytes', 'dest_bytes', 'count',
                'same_srv_rate', 'serror_rate', 'srv_serror_rate',
                'dst_host_count', 'dst_host_srv_count',
                'dst_host_same_src_port_rate', 'dst_host_serror_rate',
@@ -47,11 +47,30 @@ cols_to_std = ['duration', 'src_bytes', 'dest_bytes', 'count',
                'dst_port_num', 'start_time']
 
 # column names of categorical data
-cols_to_index = ['ashula_detection', 'dst_ip_add', 'flag', 'ids_detection', 'label',
+COLUMN_TO_INDEX = ['ashula_detection', 'dst_ip_add', 'flag', 'ids_detection', 'label',
                  'malware_detection', 'protocol', 'service', 'src_ip_add']
 
 
 def normalize_data(path, write_path, num_chunks):
+	"""Normalizes a given dataset.
+
+	Parameter
+	---------
+	path : str
+	  The path of the dataset to be normalized.
+	write_path : str
+	  The path where to save the normalized dataset.
+	num_chunks : int
+	  The number of file splits for the normalized dataset.
+
+	Example
+	-------
+	>>> PATH = '/home/data'
+	>>> WRITE_PATH = '/home/normalized_data'
+	>>> NUM_CHUNKS = 20
+	>>> normalize_data(PATH, WRITE_PATH, NUM_CHUNKS)
+	"""
+	
     # get all the CSV files in the PATH dir
     files = list_files(path=path)
 
@@ -61,13 +80,13 @@ def normalize_data(path, write_path, num_chunks):
     # append the dfs from each file to the data df
     for file in files:
         # the python engine was used to support mixed data types
-        df = df.append(pd.read_csv(filepath_or_buffer=file, names=col_names, engine='python'))
+        df = df.append(pd.read_csv(filepath_or_buffer=file, names=COLUMN_NAMES, engine='python'))
         print('Appending {}'.format(file))
 
     print('Current DataFrame shape: {}'.format(df.shape))
 
     # drop rows with NaN values
-    df[col_names] = df[col_names].dropna(axis=0, how='any')
+    df[COLUMN_NAMES] = df[COLUMN_NAMES].dropna(axis=0, how='any')
     print('DataFrame shape after NaN values removal: {}'.format(df.shape))
 
     # since malware_detection, ashula_detection,
@@ -90,14 +109,14 @@ def normalize_data(path, write_path, num_chunks):
                                                            (int(time.split(':')[2]) * (1 / 3600)))
 
     # index categorical data to [0, n-1] where n is the number of categories per feature
-    df[cols_to_index] = df[cols_to_index].apply(preprocessing.LabelEncoder().fit_transform)
+    df[COLUMN_TO_INDEX] = df[COLUMN_TO_INDEX].apply(preprocessing.LabelEncoder().fit_transform)
 
     # standardize continuous and quasi-continuous features
-    df[cols_to_std] = preprocessing.StandardScaler().fit_transform(df[cols_to_std])
+    df[COLUMN_TO_STANDARDIZE] = preprocessing.StandardScaler().fit_transform(df[COLUMN_TO_STANDARDIZE])
 
     # split the dataframe to multiple CSV files
     for id, df_i in enumerate(np.array_split(df, num_chunks)):
-        df_i.to_csv(path_or_buf=os.path.join(write_path, '{id}.csv'.format(id=id)), columns=col_names, header=None,
+        df_i.to_csv(path_or_buf=os.path.join(write_path, '{id}.csv'.format(id=id)), columns=COLUMN_NAMES, header=None,
                     index=False)
         print('Saving CSV file : {path}'.format(path=os.path.join(write_path, '{id}'.format(id=id))))
 
