e614bb137f26c87210fc0b08e3b64c57850670cd
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-29 20:55:56 +0800

    Allows user to choose an operation, i.e. training or testing.

--- run.sh
+++ run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-python3 gru_svm_main.py --train_dataset dataset/train/train_data.npy \
---validation_dataset dataset/test/test_data.npy \
---checkpoint_path models/checkpoint/gru_svm \
---model_name gru_svm.ckpt \
---log_path models/logs/gru_svm \
---result_path results/gru_svm
+figlet "GRU-SVM"
+printf "Operation: [1] Train [2] Test\n>> "
+read choice
+if [ "$choice" -eq "1" ] &>/dev/null; then
+	echo "Training GRU-SVM for intrusion detection"
+	python3 gru_svm_main.py --operation "train" \
+	--train_dataset dataset/train/train_data.npy \
+	--validation_dataset dataset/test/test_data.npy \
+	--checkpoint_path models/checkpoint/gru_svm \
+	--model_name gru_svm.ckpt \
+	--log_path models/logs/gru_svm \
+	--result_path results/gru_svm
+elif [ "$choice" -eq "2" ] &>/dev/null; then
+	if [ -d "./models/checkpoint/gru_svm" ] &>/dev/null; then
+		echo "Testing GRU-SVM for intrusion detection"
+		python3 gru_svm_main.py --operation "test" \
+		--validation_dataset dataset/test/test_data.npy \
+		--checkpoint_path models/checkpoint/gru_svm \
+		--result_path results/gru_svm
+	else
+		echo "Train the model first!"
+		exit
+	fi
+else
+	echo "Invalid input"
+	exit
+fi
