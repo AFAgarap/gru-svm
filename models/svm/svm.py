08ae588153b965ab97cff76ea2edd293123b8d01
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-29 22:01:09 +0800

    Increments the class version; forgot to do so in the previous commit

--- models/svm/svm.py
+++ models/svm/svm.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __author__ = 'Abien Fred Agarap'
 
 import numpy as np
